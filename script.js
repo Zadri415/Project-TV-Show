ac6033e38b65f19f8c026b79a60bc4b8f7a7c088
Zadri Abdule <zadriabdule@gmail.com>
2026-05-13 23:48:03 +0100

    add setup for episode rendering

--- script.js
+++ script.js
@@ -6,7 +6,30 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  rootElem.innerHTML = "";
+
+  episodeList.forEach((episode) => {
+    const card = document.createElement("article");
+
+    const episodeCode = `S${String(episode.season).padStart(
+      2,
+      "0",
+    )}E${String(episode.number).padStart(2, "0")}`;
+
+    card.innerHTML = `
+      <h2>${episode.name} - ${episodeCode}</h2>
+
+      <img 
+        src="${episode.image.medium}" 
+        alt="${episode.name}"
+      />
+
+      <p>${episode.summary}</p>
+    `;
+
+    rootElem.appendChild(card);
+  });
 }
 
 window.onload = setup;
