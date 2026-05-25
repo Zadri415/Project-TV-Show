29a66e1f101d3d03e0a62e8f161f7262e890f76d
Zadri Abdule <zadriabdule@gmail.com>
2026-05-25 14:39:46 +0100

    added episode page structure

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 //You can edit ALL of the code here
 function setup() {
+  // grab all the episodes from the data file
   const allEpisodes = getAllEpisodes();
+  // pass them into our function so we can display them
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
+  // find the root div in HTML (this is where episodes will go)
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  // clear anything already inside root 
+  rootElem.innerHTML = "";
+
+  // go through each episode one by one
+  episodeList.forEach(function (episode) {
+    // make the episode code look like S01E07
+    // padStart adds a 0 if the number is only 1 digit
+    const episodeCode = "S" + String(episode.season).padStart(2, "0") + "E" + String(episode.number).padStart(2, "0");
+
+    // put the episode info inside the card 
+    Clipboard.innerHTML = `
+      <div class="card">
+        <h2>${episode.name} - ${episodeCode}</h2>
+        <img src="${episode.image.medium}" alt="${episode.name}">
+        <p>${episode.summary}</p>
+      </div>
+    `;
+    // add the card to the page
+    rootElem.appendChild(card);
+  });
+
 
 window.onload = setup;
