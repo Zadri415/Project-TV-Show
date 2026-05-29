61d68cc92f7683aef2fe35fc78f22db3037b07f9
Alex Jamshidi <alexjamshidi@gmail.com>
2026-05-29 03:16:40 +0100

    refactored, addEpisode made into seperate function, clone made to use template

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // DOMS
 const rootElem = document.getElementById("root");
+const episodeTemplate = document.getElementById("episode-template");
 
 function setup() {
   // grab all the episodes from the data file
@@ -13,27 +14,31 @@ function makePageForEpisodes(episodeList) {
   rootElem.innerHTML = "";
 
   // go through each episode one by one
-  episodeList.forEach(function (episode) {
-    const episodeCode = createEpisodeCode(episode);
-    const card = document.createElement("article");
-    // put the episode info inside the card
-    card.innerHTML = `
-        <h2>${episode.name} - ${episodeCode}</h2>
-        <img src="${episode.image.medium}" alt="${episode.name}">
-        <p>${episode.summary}</p>
-      `;
-    // add the card to the page
-    rootElem.appendChild(card);
-  });
+  episodeList.forEach(addEpisode);
+}
+
+function addEpisode(episode) {
+  const episodeCode = createEpisodeCode(episode);
+  const clone = episodeTemplate.content.cloneNode(true);
+
+  // put the episode info inside the card
+  clone.querySelector("h2").textContent = `${episode.name} - ${episodeCode}`;
+  clone.querySelector("img").src = episode.image.medium;
+  clone.querySelector("p").textContent = episode.summary;
+
+  // add the card to the page
+  rootElem.appendChild(clone);
 }
 
 // make the episode code look like S01E07
 // padStart adds a 0 if the number is only 1 digit
 function createEpisodeCode(episode) {
-  "S" +
+  return (
+    "S" +
     String(episode.season).padStart(2, "0") +
     "E" +
-    String(episode.number).padStart(2, "0");
+    String(episode.number).padStart(2, "0")
+  );
 }
 
 window.onload = setup;
